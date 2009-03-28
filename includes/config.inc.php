2c9f5e3588e255f87cc29a286c5b53a813d4c720
renlok <renlok@cb2008ec-e0ba-457b-a47b-93945ab2bf72>
2009-03-28 12:15:03 +0000

    Fixed the config file
    git-svn-id: svn://svn.code.sf.net/p/simpleauction/svn/WeBid/trunk@26 cb2008ec-e0ba-457b-a47b-93945ab2bf72

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,9 +1,3 @@
 <?php
-$DbHost     = "localhost";
-$DbDatabase = "auction";
-$DbUser     = "root";
-$DbPassword = "";
-$DBPrefix	= "webid_";
-$main_path	= "C:\\xampplite\\htdocs\\WeBid\\";
-//echo '<script type="text/javascript">window.location = "install/install.php";</script>';
+echo '<script type="text/javascript">window.location = "install/install.php";</script>';
 ?>

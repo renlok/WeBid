4be61471c201eb17f48100faaea3580e8e9af1e4
renlok <renlok@cb2008ec-e0ba-457b-a47b-93945ab2bf72>
2009-10-01 19:09:54 +0000

    Replaced index file
    git-svn-id: svn://svn.code.sf.net/p/simpleauction/svn/WeBid/trunk@182 cb2008ec-e0ba-457b-a47b-93945ab2bf72

--- index.php
+++ index.php
@@ -1,10 +1,252 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Untitled Document</title>
-</head>
+<?php
+/***************************************************************************
+ *   copyright				: (C) 2008, 2009 WeBid
+ *   site					: http://www.webidsupport.com/
+ ***************************************************************************/
 
-<body>
-</body>
-</html>
+/***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version. Although none of the code may be
+ *   sold. If you have been sold this script, get a refund.
+ ***************************************************************************/
+
+include 'includes/common.inc.php';
+include $main_path . 'language/' . $language . '/categories.inc.php';
+
+// Run cron according to SETTINGS
+if ($system->SETTINGS['cron'] == 2)
+{
+	include_once 'cron.php';
+}
+
+if ($system->SETTINGS['loginbox'] == 1 && $system->SETTINGS['https'] == 'y' && $_SERVER['HTTPS'] != 'on')
+{
+	$sslurl = str_replace('http://', 'https://', $system->SETTINGS['siteurl']);
+	header('Location: ' . $sslurl . 'index.php');
+	exit;
+}
+
+$NOW = time();
+
+function ShowFlags()
+{
+	global $system, $LANGUAGES;
+	$counter = 0;
+	$flags = '';
+	foreach ($LANGUAGES as $lang => $value)
+	{
+		if ($counter > 3)
+		{
+			$flags .= '<br>';
+			$counter = 0;
+		}
+		$flags .= '<a href="?lan=' . $lang . '"><img vspace="2" hspace="2" src="' . $system->SETTINGS['siteurl'] . 'includes/flags/' . $lang . '.gif" border="0" alt="' . $lang . '"></a>';
+		$counter++;
+	}
+	return $flags;
+}
+
+// prepare categories list for templates/template
+// Prepare categories sorting
+if ($system->SETTINGS['catsorting'] == 'alpha')
+{
+	$catsorting = ' ORDER BY cat_name ASC';
+}
+else
+{
+	$catsorting = ' ORDER BY sub_counter DESC';
+}
+
+$query = "SELECT cat_id FROM " . $DBPrefix . "categories WHERE parent_id = -1";
+$res = mysql_query($query);
+$system->check_mysql($res, $query, __LINE__, __FILE__);
+
+$query = "SELECT * FROM " . $DBPrefix . "categories
+		  WHERE parent_id = " . mysql_result($res, 0) . "
+		  " . $catsorting . "
+		  LIMIT " . $system->SETTINGS['catstoshow'];
+$res = mysql_query($query);
+$system->check_mysql($res, $query, __LINE__, __FILE__);
+
+while ($row = mysql_fetch_assoc($res))
+{
+	$template->assign_block_vars('cat_list', array(
+			'CATAUCNUM' => ($row['sub_counter'] != 0) ? '(' . $row['sub_counter'] . ')' : '',
+			'ID' => $row['cat_id'],
+			'IMAGE' => (!empty($row['cat_image'])) ? '<img src="' . $row['cat_image'] . '" border=0>' : '',
+			'COLOUR' => (empty($row['cat_colour'])) ? '#FFFFFF' : $row['cat_colour'],
+			'NAME' => $category_names[$row['cat_id']]
+			));
+}
+
+// get featured items
+$query = "SELECT id, title, current_bid, pict_url, ends, num_bids, minimum_bid
+        FROM " . $DBPrefix . "auctions
+        WHERE closed = 0 AND suspended = 0 AND starts <= " . $NOW . "
+		AND featured = 'y'
+        ORDER BY RAND() DESC LIMIT 12";
+$res = mysql_query($query);
+$system->check_mysql($res, $query, __LINE__, __FILE__);
+while($row = mysql_fetch_assoc($res))
+{
+	$ends = $row['ends'];
+	$difference = $ends - time();
+	if ($difference > 0)
+	{
+		$ends_string = FormatTimeLeft($difference);
+	}
+	else
+	{
+		$ends_string = $MSG['911'];
+	}
+	$high_bid = ($row['num_bids'] == 0) ? $row['minimum_bid'] : $row['current_bid'];
+	$template->assign_block_vars('featured', array(
+			'ENDS' => $ends_string,
+			'ID' => $row['id'],
+			'BID' => $system->print_money($high_bid),
+			'IMAGE' => (!empty($row['pict_url'])) ? 'getthumb.php?w=' . $system->SETTINGS['thumb_show'] . '&fromfile=' . $uploaded_path . $row['id'] . '/' . $row['pict_url'] : 'images/email_alerts/default_item_img.jpg',
+			'TITLE' => $row['title']
+			));
+}
+
+// get last created auctions
+$query = "SELECT id, title, starts from " . $DBPrefix . "auctions
+		 WHERE closed = 0 AND suspended = 0
+		 AND starts <= " . $NOW . "
+		 ORDER BY starts DESC
+		 LIMIT " . $system->SETTINGS['lastitemsnumber'];
+$res = mysql_query($query);
+$system->check_mysql($res, $query, __LINE__, __FILE__);
+
+$i = 0;
+$bgcolor = '#FFFFFF';
+while ($row = mysql_fetch_assoc($res))
+{
+	$template->assign_block_vars('auc_last', array(
+			'BGCOLOUR' => ($bgcolor == '#FFFFFF') ? '#FFFEEE' : '#FFFFFF',
+			'DATE' => ArrangeDateNoCorrection($row['starts'] + $system->tdiff),
+			'ID' => $row['id'],
+			'TITLE' => $row['title']
+			));
+	$i++;
+}
+
+$auc_last = ($i > 0) ? true : false;
+// get ending soon auctions
+$query = "SELECT ends, id, title FROM " . $DBPrefix . "auctions
+		 WHERE closed = 0 AND suspended = 0 AND starts <= " . $NOW . "
+		 ORDER BY ends LIMIT " . $system->SETTINGS['endingsoonnumber'];
+$res = mysql_query($query);
+$system->check_mysql($res, $query, __LINE__, __FILE__);
+
+$i = 0;
+$bgcolor = '#FFFFFF';
+while ($row = mysql_fetch_assoc($res))
+{
+	$difference = $row['ends'] - time();
+	if ($difference > 0)
+	{
+		$ends_string = FormatTimeLeft($difference);
+	}
+	else
+	{
+		$ends_string = $MSG['911'];
+	}
+	$template->assign_block_vars('end_soon', array(
+			'BGCOLOUR' => ($bgcolor == '#FFFFFF') ? '#FFFEEE' : '#FFFFFF',
+			'DATE' => $ends_string,
+			'ID' => $row['id'],
+			'TITLE' => $row['title']
+			));
+	$i++;
+}
+
+$end_soon = ($i > 0) ? true : false;
+// get higher bids
+$query = "SELECT max(b.bid) AS max_bid, a.title, a.id FROM " . $DBPrefix . "bids b
+		LEFT JOIN " . $DBPrefix . "auctions a ON (a.id = b.auction)
+		WHERE a.suspended = 0 AND a.closed = 0 AND a.starts <= '" . $NOW . "'
+		GROUP BY b.bid, b.auction ORDER BY max_bid DESC
+		LIMIT " . $system->SETTINGS['higherbidsnumber'];
+$res = mysql_query($query);
+$system->check_mysql($res, $query, __LINE__, __FILE__);
+
+$i = $j = 0;
+$bgcolor = '#FFFFFF';
+$AU = array();
+while ($row = mysql_fetch_assoc($res))
+{
+	if (!in_array($row['id'], $AU))
+	{
+		$template->assign_block_vars('max_bids', array(
+				'BGCOLOUR' => ($bgcolor == '#FFFFFF') ? '#FFFEEE' : '#FFFFFF',
+				'FBID' => $system->print_money($row['max_bid']),
+				'BID' => $row['max_bid'],
+				'ID' => $row['id'],
+				'TITLE' => $row['title']
+				));
+		$AU[] = $row['id'];
+		$j++;
+	}
+	$i++;
+}
+$high_bids = ($j > 0) ? true : false;
+// Build list of help topics
+$query = "SELECT id, category FROM " . $DBPrefix . "faqscat_translated WHERE lang = '" . $language . "' ORDER BY category ASC";
+$res = mysql_query($query);
+$system->check_mysql($res, $query, __LINE__, __FILE__);
+$i = 0;
+while ($faqscat = mysql_fetch_assoc($res))
+{
+	$template->assign_block_vars('helpbox', array(
+			'ID' => $faqscat['id'],
+			'TITLE' => $faqscat['category']
+			));
+	$i++;
+}
+
+$helpbox = ($i > 0) ? true : false;
+// Build news list
+if ($system->SETTINGS['newsbox'] == 1)
+{
+	$query = "SELECT n.title As t, n.new_date, t.* FROM " . $DBPrefix . "news n
+			LEFT JOIN " . $DBPrefix . "news_translated t ON (t.id = n.id)
+			WHERE t.lang = '" . $language . "' AND n.suspended = 0
+			ORDER BY new_date DESC, id DESC LIMIT " . $system->SETTINGS['newstoshow'];
+	$res = mysql_query($query);
+	$system->check_mysql($res, $query, __LINE__, __FILE__);
+	while ($new = mysql_fetch_assoc($res))
+	{
+		$template->assign_block_vars('newsbox', array(
+				'ID' => $new['id'],
+				'DATE' => FormatDate($new['new_date']),
+				'TITLE' => (!empty($new['title'])) ? $new['title'] : $new['t']
+				));
+	}
+}
+
+$template->assign_vars(array(
+		'FLAGS' => ShowFlags(),
+		'LOGIN_ERROR' => (isset($_SESSION['loginerror'])) ? $_SESSION['loginerror'] : '',
+
+		'B_AUC_LAST' => $auc_last,
+		'B_HIGH_BIDS' => $high_bids,
+		'B_AUC_ENDSOON' => $end_soon,
+		'B_HELPBOX' => $helpbox,
+		'B_MULT_LANGS' => (count($LANGUAGES) > 1),
+		'B_LOGIN_BOX' => ($system->SETTINGS['loginbox'] == 1),
+		'B_LOGIN_ERROR' => (isset($_SESSION['loginerror'])),
+		'B_NEWS_BOX' => ($system->SETTINGS['newsbox'] == 1)
+		));
+
+include 'header.php';
+$template->set_filenames(array(
+		'body' => 'home.tpl'
+		));
+$template->display('body');
+include 'footer.php';
+
+unset($_SESSION['loginerror']);
+?>

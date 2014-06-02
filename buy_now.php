c63a4bfa893bfe133b06b797969b134534feb61d
Jason <ubidzz@live.com>
2014-06-02 16:58:39 -0400

    Paying for multiple items (FIX)
    You can find more details about this at the link below
    
    http://www.webidsupport.com/forums/issue.php?472-Paying-for-multiple-items-(FIX)

--- buy_now.php
+++ buy_now.php
@@ -296,6 +296,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'buy')
 		$buy_done = 1;
 	}
 }
+$id = $db->lastInsertId();
 
 $additional_shipping = $Auction['additional_shipping_cost'] * ($qty - 1);
 $shipping_cost = ($Auction['shipping'] == 1) ? ($Auction['shipping_cost'] + $additional_shipping) : 0;
@@ -304,6 +305,7 @@ $BN_total = ($Auction['bid'] * $qty) + $shipping_cost;
 $template->assign_vars(array(
 		'ERROR' => (isset($ERR)) ? $ERR : '',
 		'ID' => $_REQUEST['id'],
+		'WINID' => $id,
 		'TITLE' => $Auction['title'],
 		'BN_PRICE' => $system->print_money($Auction['buy_now']),
 		'BN_TOTAL' => $system->print_money($BN_total),

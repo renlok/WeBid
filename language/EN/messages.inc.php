0c03d045dd9022585fe7d04287fd2c24a1e7ef63
pani100 <pani200@gmail.com>
2014-10-14 13:38:34 +0100

    Update messages.inc.php
    Adding error messages for shipping and additional shipping incorrect currency format

--- language/EN/messages.inc.php
+++ language/EN/messages.inc.php
@@ -101,6 +101,8 @@ $ERR_075 = 'No bids';
 $ERR_076 = 'Invalid Username/E-mail combination';
 $ERR_077 = 'Valid Token Expired';
 $ERR_078 = 'You must agree to the Terms and Conditions';
+$ERR_079 = 'The Shipping fee price you inserted is not correct';
+$ERR_080 = 'Additional Shipping price you inserted is not correct';
 
 $ERR_100 = "User does not exist";
 $ERR_101 = "Password incorrect";

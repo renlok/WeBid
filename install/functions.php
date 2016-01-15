cfc3b98c3b8c473614134f1af2032dff6fde18f1
Nozlaf <nozlaf@nozlaf.com>
2016-01-15 21:09:37 +1100

    BC math support was being checked twice, also fixed the PDO TD to not have colspan=2 to match other checks

--- install/functions.php
+++ install/functions.php
@@ -349,11 +349,9 @@ function show_config_table($fresh = true)
 		$data .= (extension_loaded('bcmath')) ? '<strong style="color:green">Found</strong>' : '<strong style="color:red">Not Found</strong>';
 		$data .= '</tr>';
 
-		$data .= '<tr><td colspan="2">BC Math Support:</td><td colspan="2">';
-		$data .= (extension_loaded('bcmath')) ? '<strong style="color:green">Found</strong>' : '<strong style="color:red">Not Found</strong>';
-		$data .= '</tr>';
 
-		$data .= '<tr><td colspan="2">PHP Data Objects Support:</td><td colspan="2">';
+
+		$data .= '<tr><td>PHP Data Objects Support:</td><td colspan="2">';
 		$data .= (extension_loaded('pdo')) ? '<strong style="color:green">Found</strong>' : '<strong style="color:red">Not Found</strong>';
 		$data .= '</tr>';
 

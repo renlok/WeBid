b328c41e02340299c48d39238c58694884e7655c
Nozlaf <nozlaf@nozlaf.com>
2016-09-23 10:44:36 +1000

    updated recapcha from the google repo
    Mostly spelling has been fixed
    one small change to socket.php

--- includes/packages/recaptcha/ReCaptcha/ReCaptcha.php
+++ includes/packages/recaptcha/ReCaptcha/ReCaptcha.php
@@ -39,12 +39,12 @@ class ReCaptcha
 
     /**
      * Shared secret for the site.
-     * @var type string
+     * @var string
      */
     private $secret;
 
     /**
-     * Method used to communicate  with service. Defaults to POST request.
+     * Method used to communicate with service. Defaults to POST request.
      * @var RequestMethod
      */
     private $requestMethod;
@@ -54,6 +54,7 @@ class ReCaptcha
      *
      * @param string $secret shared secret between site and reCAPTCHA server.
      * @param RequestMethod $requestMethod method used to send the request. Defaults to POST.
+     * @throws \RuntimeException if $secret is invalid
      */
     public function __construct($secret, RequestMethod $requestMethod = null)
     {
